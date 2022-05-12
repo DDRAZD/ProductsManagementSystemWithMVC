a156cf54263f2eb39d329af79298ed1d59c3b3d6
Yaniv Nathan <yanivnathan@hotmail.com>
2022-05-12 07:15:21 -0400

    added an assignment from udemy on denominations

--- ProductsManagementSystemWithMVC/Controllers/HomeController.cs
+++ ProductsManagementSystemWithMVC/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace ProductsManagementSystemWithMVC.Controllers
         // GET: Home
         public ActionResult Index()
         {
+           
+            
             return View();
         }
 
@@ -77,6 +79,56 @@ namespace ProductsManagementSystemWithMVC.Controllers
             ViewBag.Marks = 20;
             ViewBag.NoOfSemesters = 6;
             ViewBag.Subjects = new List<string>() { "Math", "Physics", "Chemistry" };
+            return View();
+        }
+        /// <summary>
+        /// this is from the Udemy assignment 2 on razors; recevices an amount and breaks it down to denomiations
+        /// Consider, you have denominations of 1000, 500, 100, 50, 10, 5, and 1.
+        /// </summary>
+        /// <param name="Amount">received from the user from the url</param>
+        /// <returns>the view that will have razor block in it</returns>
+        public ActionResult IndexAssignment(int Amount)
+        {
+            List<int> numbersList = new List<int>();
+            int numberToAdd = 0;
+            numberToAdd = Amount % 10;
+
+            if (Amount % 10 >= 5)
+            {
+                numbersList.Add(numberToAdd % 5); // adds the 1s
+                numbersList.Add(1); // the only case you will actually add denomination of 5
+            }
+            else
+            {
+                numbersList.Add(numberToAdd); // adds the 1s
+                numbersList.Add(0);//adds denomination of 5 as 0
+
+            }
+
+            Amount = Amount / 10;//removes the 1st 
+            numberToAdd = Amount % 10;//gets the 10s
+            numbersList.Add(numberToAdd);//adds the 10
+            Amount = Amount / 10; //removes the 10st
+            numberToAdd = Amount % 10;//gets the 100s
+            if (Amount % 10 >= 5)
+            {
+                numbersList.Add(numberToAdd % 5); // adds the 100s
+                numbersList.Add(1); // the only case you will actually add denomination of 500
+            }
+            else
+            {
+                numbersList.Add(numberToAdd); // adds the 100s
+                numbersList.Add(0);//adds denomination of 5 as 0
+
+            }
+
+            Amount = Amount / 10; //removes the 100
+            numbersList.Add(Amount);//adds the 1000s
+
+
+            ViewBag.Numbers = numbersList;
+
+
             return View();
         }
     }
