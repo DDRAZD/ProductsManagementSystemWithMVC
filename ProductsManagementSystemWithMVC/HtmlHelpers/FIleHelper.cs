a87f924fedd443f808f61e5da8956c4824895f17
Yaniv Nathan <yanivnathan@gmail.com>
2022-06-03 07:02:28 -0400

    add customer htmlHelpers to deploy for File uploads and future other functions

---
+++ ProductsManagementSystemWithMVC/HtmlHelpers/FIleHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductsManagementSystemWithMVC.HtmlHelpers
+{
+    public static class FIleHelper
+    {
+        public static MvcHtmlString File(this HtmlHelper htmlHelper, string CssClassname)
+        {
+            TagBuilder tag = new TagBuilder("input");
+            tag.MergeAttribute("type", "file");
+            tag.MergeAttribute("id", "image");
+            tag.MergeAttribute("name", "Photo");
+            tag.MergeAttribute("class", CssClassname);
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.SelfClosing));//self closing as this is going to be an <input> tag
+        }
+
+    }
+}
