f347be34942982de695bdec57254b9326923b118
Yaniv Nathan <yanivnathan@gmail.com>
2022-06-24 07:22:50 -0400

    added an ability for admin to see list of users

---
+++ ProductsManagementSystemWithMVC/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductsManagementSystemWithMVC.Identity;
+
+namespace ProductsManagementSystemWithMVC.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            List<ApplicationUser> existingUsers = db.Users.ToList();//pulling all user information from DB
+
+
+            return View(existingUsers);
+        }
+    }
+}
