b6c3f2f05fc7376493caaf2dcee8bef6ab824a7c
Yaniv Nathan <yanivnathan@gmail.com>
2022-08-03 07:28:26 -0400

    added webapi and get method for brands + a notes document to keep track

---
+++ ProductsManagementSystemWithMVC/ApiControllers/BrandsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Company.DomainModels;
+using Company.DataLayer;
+
+namespace ProductsManagementSystemWithMVC.ApiControllers
+{
+    public class BrandsController : ApiController
+    {
+        public List<Brand> Get()
+        {
+            CompanyDbContext db = new CompanyDbContext();
+            List<Brand> brands = new List<Brand>();
+            brands = db.Brands.ToList();
+            return brands;
+        }
+    }
+}
--- ProductsManagementSystemWithMVC/Global.asax.cs
+++ ProductsManagementSystemWithMVC/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using Company.DomainModels;
 using System.IO;
+using System.Web.Http;
 
 namespace ProductsManagementSystemWithMVC
 {
@@ -14,11 +15,14 @@ namespace ProductsManagementSystemWithMVC
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
          //   ModelBinders.Binders.Add(typeof(Student), new CustomBinder());
 
             //calling the filter config
             FilterConfig.RegisterGlobalFiters(GlobalFilters.Filters);//this argument we are passing here will be recevied in FilterConfig.cs
+
+           
         }
 
         protected void Application_Error()
